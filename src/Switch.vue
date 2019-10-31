d6e78655dd0741a90d720851eb8db56b6e11f69d
Steven Duong <stevenduong1983tx@gmail.com>
2019-10-31 10:45:01 -0700

    Done

---
+++ src/Switch.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <div id="on"
+             @click="switched(true)"
+            :class="{active : value}"
+        > On
+        </div>
+
+        <div id="off"
+             @click="switched(false)"
+             :class="{active : !value}"
+        > Off
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+       props:['value'],
+        methods:{
+           switched(isOn){
+               this.$emit('input',isOn);
+           }
+        }
+    }
+</script>
+<style scoped>
+    #on,#off{
+        width:40px;
+        height:20px;
+        display:inline-block;
+        background-color:lightgray;
+        cursor:pointer;
+        padding:2px;
+        margin:10px -2px;
+        box-sizing:content-box;
+        text-align: center;
+    }
+
+    #on.hover,#on.active{
+        background-color:lightgreen;
+    }
+
+    #off.hover,#off.active{
+        background-color:lightcoral;
+    }
+
+</style>
